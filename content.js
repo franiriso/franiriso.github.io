4af39ff428bb9ccb1f7e6cab3946a2ad38ffa879
Fran Iriso <franiriso@gmail.com>
2026-04-08 02:26:24 +0200

    wip

--- content.js
+++ content.js
@@ -295,7 +295,7 @@ export const portfolioContent = {
         image: {
           src: "assets/pcb_project_cutout_rotated.png",
           alt: "Rotated PCB project cutout",
-          tilt: "-50deg"
+          tilt: "20deg"
         },
         // mediaLabel: "Add PCB render or lab photo",
         links: [
